e323f64a7b62ec2dc2d304e3b771d1220659a873
System Administrator <root@shreyas-mbp-3.dyndns.rice.edu>
2024-01-30 16:07:05 -0600

    LCD print added

--- test/test.ino
+++ test/test.ino
@@ -3,11 +3,13 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 
 void setup() {
-  // put your setup code here, to run once:
-// hello world pls save
+lcd.begin(16, 2); //Initiate the LCD in a 16x2 configuration
+lcd.print("My name is:");
+lcd.setCursor(0, 1);
+lcd.print("shreya jindal");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+delay(50);
 }
